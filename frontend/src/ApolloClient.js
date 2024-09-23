23f76c69a2e74e25cdbf41c1cc72701d6bb6dfea
bom254 <bevonmokuas@gmail.com>
2024-09-23 17:30:55 +0000

    Initialize subgraph

--- frontend/src/ApolloClient.js
+++
@@ -1,9 +0,0 @@
-// src/ApolloClient.js
-import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:8000/subgraphs/name/alx-example', // Replace with your subgraph name
-  cache: new InMemoryCache(),
-});
-
-export default client;
